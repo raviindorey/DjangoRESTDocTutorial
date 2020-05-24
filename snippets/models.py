dbc39e7a16eb9c24f63612ee36d70c78f22d4446
Ravi Indorey <ravi.indorey90@gmail.com>
2020-05-24 19:34:33 +0530

    Add owner and highlighted fields

--- snippets/models.py
+++ snippets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -17,6 +19,22 @@ class Snippet(models.Model):
                                 max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly',
                              max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='snippets',
+                              on_delete=models.CASCADE)
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ['created']
+
+    def save(self, *args, **kwargs):
+        """
+        Use pygment library to create highlighted HTML representation of the
+        code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.lineos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(linenos=linenos, style=self.style,
+                                  full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
