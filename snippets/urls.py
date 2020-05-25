3e0b8d71824e0b9201a2db31b52c99f8ec6c00eb
Ravi Indorey <ravi.indorey90@gmail.com>
2020-05-25 18:09:18 +0530

    View for snippet highlight

--- snippets/urls.py
+++ snippets/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('', views.api_root),
--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,7 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
 from snippets.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
 from snippets.models import Snippet
@@ -33,6 +34,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
             permissions.IsAuthenticatedOrReadOnly,
             IsOwnerOrReadOnly,
         ]
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class UserList(generics.ListAPIView):
