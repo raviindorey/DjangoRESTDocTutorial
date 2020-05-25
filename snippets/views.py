ef4f2673a749bc390b20d7d76aef65516a27f588
Ravi Indorey <ravi.indorey90@gmail.com>
2020-05-25 17:43:31 +0530

    Api root

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,20 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from snippets.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
+
+
+@api_view
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
